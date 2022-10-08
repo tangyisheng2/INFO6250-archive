1c216e8365226732ff9e217d2b2ed9536a20d85b
tangyisheng2 <tangyisheng2@sina.com>
2022-10-07 17:33:51 -0700

    feat: <basic-express> enable read and send new message

--- work/basic-express/chat-web.js
+++ work/basic-express/chat-web.js
@@ -1,14 +1,16 @@
 const chatWeb = {
-  chatPage: function(chat) {
+  chatPage: function (chat) {
     // Fill in/modify anything below!
     return `
       <!doctype html>
       <html>
         <head>
           <title>Chat</title>
+          <link href="styles.css" rel="stylesheet">
         </head>
         <body>
           <div id="chat-app">
+            <h1>Node Chat</h1>
             ${chatWeb.getUserList(chat)}
             ${chatWeb.getMessageList(chat)}
             ${chatWeb.getOutgoing(chat)}
@@ -18,26 +20,56 @@ const chatWeb = {
   `;
   },
 
-  getMessageList: function(chat) {
-    return `<ol class="messages">` +
-      // Fill in
-      // Generate the HTML for the list of messages
-      `</ol>`;
+  getMessageList: function (chat) {
+    return (
+      `<ol class="messages">` +
+      chat.messages
+        .map(
+          (message) =>
+            `<li class="messages__item">${message.sender}: ${message.text}</li>`
+        )
+        .join("") +
+      `</ol>`
+    );
   },
-  getUserList: function(chat) {
-    return `<ul class="users">` +
-    Object.values(chat.users).map( user => `
+  getUserList: function (chat) {
+    return (
+      `<ul class="users">` +
+      Object.values(chat.users)
+        .map(
+          (user) => `
       <li>
         <div class="user">
           <span class="username">${user}</span>
         </div>
       </li>
-    `).join('') +
-    `</ul>`;
+    `
+        )
+        .join("") +
+      `</ul>`
+    );
   },
-  getOutgoing: function() {
-    // Fill in
-    // Generate the HTML for a form to send a message
-  }
+  getOutgoing: function () {
+    return `      
+      <div class="sender-field">
+        <form action="/chat" method="post" class="sender-form">
+          <label>Your Name: </label>
+          <input
+            class="sender-data"
+            name='username'
+            type="text"
+            placeholder="anonymous"
+          />
+          <label>Input text: </label>
+          <input
+            class="sender-data"
+            name='text'
+            type="text"
+            placeholder="New Messages"
+          />
+          <button type="submit">Send</button>
+        </form>
+      </div>`;
+  },
 };
 module.exports = chatWeb;
