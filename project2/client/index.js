778b9f232e7399db85bb590a8ced2b189b7791ab
tangyisheng2 <tangyisheng2@sina.com>
2022-11-13 23:47:37 -0800

    feat: <project2> explicitly set timeout to show "loading" indicator

--- project2/client/index.js
+++ project2/client/index.js
@@ -1,15 +1,17 @@
-'use strict';
+"use strict";
 const {
   initEventListener,
   fetchSession,
   fetchChat,
   initPolling,
-} = require('./src/controller');
-const render = require('./src/react');
+} = require("./src/controller");
+const render = require("./src/react");
 
-fetchSession()
-  .then(() => fetchChat())
-  .then(() => render());
-render();
-initEventListener();
-initPolling();
+setTimeout(() => {
+  fetchSession()
+    .then(() => fetchChat())
+    .then(() => render());
+  render();
+  initEventListener();
+  initPolling();
+}, 1000); // Explicit set timeout to show "Loading" indicator
