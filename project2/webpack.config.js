cf96726a94378815228d3fa8a701a6f83b97824d
tangyisheng2 <tangyisheng2@sina.com>
2022-11-13 22:56:28 -0800

    feat: <project2> update config for package.json and webpack

--- project2/webpack.config.js
+++ project2/webpack.config.js
@@ -1,16 +1,23 @@
-const path = require("path");
+const path = require('path');
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  devtool: "source-map",
+  mode: 'development',
+  entry: './client/index.js',
+  devtool: 'source-map',
   devServer: {
-    static: path.join(__dirname, "public"),
+    static: path.join(__dirname, 'public'),
     compress: true,
-    port: 5001,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3003',
+        router: () => 'http://localhost:3001',
+        logLevel: 'debug',
+      },
+    },
   },
   output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "public"),
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
@@ -18,8 +25,8 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-          options: { presets: ["@babel/preset-env"] },
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] },
         },
       },
     ],
