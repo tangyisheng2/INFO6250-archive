127b9140ba5ab0405a14f16e3d28b327f07f4ded
Eason Tang <tangyisheng2@sina.com>
2022-12-04 14:17:44 -0800

    fix: <final> server: change userId to String

--- final/server/src/storage.js
+++ final/server/src/storage.js
@@ -12,14 +12,16 @@ const storage = {
   }, // {userId: {name, postId, commentId}}
   post: {
     1: {
-      userId: 1,
+      userId: "1",
       title: "Hello World!",
       content: "This is your first post",
       cover: "",
       likeCount: 0,
     },
   }, // {postId: userId, title, content, cover}
-  commentForPost: {}, // {postId: {commentId: content, userId}}
+  commentForPost: {
+    1: { 1: { content: "Hello first content!", userId: "1" } },
+  }, // {postId: {commentId: content, userId}}
 };
 
 module.exports = storage;
