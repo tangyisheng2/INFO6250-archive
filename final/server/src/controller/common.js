616bcd222e3e510ad05d7bb188d17dc4d7b76505
Eason Tang <tangyisheng2@sina.com>
2022-12-04 00:05:35 -0800

    feat: <final> server: add common utils in controller

---
+++ final/server/src/controller/common.js
@@ -0,0 +1,8 @@
+const uuid = require("uuid").v4;
+
+function getRandomID(length) {
+  const uuidString = uuid().toString();
+  return uuidString.substring(uuidString.length - length);
+}
+
+module.exports = { getRandomID };
