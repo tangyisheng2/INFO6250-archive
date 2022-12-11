934fcd12eec169bb49a848c539200c2f66658254
Eason Tang <tangyisheng2@sina.com>
2022-12-10 23:39:36 -0800

    feat: <final> client: add "new comment" featrue

--- final/client/src/components/PostComment.jsx
+++ final/client/src/components/PostComment.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { fetchCommentForPost } from "../controller/comment-controller";
+import {
+  addCommentForPost,
+  fetchCommentForPost,
+} from "../controller/comment-controller";
 
 function PostComment({ postId }) {
   const [commentList, setCommentList] = useState([]);
@@ -10,8 +13,19 @@ function PostComment({ postId }) {
     setCommentExpanded(!commentExpanded);
   }
 
+  function onSubmitComment(e) {
+    e.preventDefault();
+    const content = e.target["comment-new"].value;
+    addCommentForPost(postId, content).then((res) =>
+      setCommentList([res, ...commentList])
+    );
+  }
+
   useEffect(() => {
-    fetchCommentForPost(postId).then((res) => setCommentList(res));
+    fetchCommentForPost(postId).then((res) => {
+      console.log(commentList, res);
+      return setCommentList(res);
+    });
   }, []);
 
   return (
@@ -20,19 +34,33 @@ function PostComment({ postId }) {
         {commentExpanded ? "Comment ✖" : "Comment ☰"}
       </button>
       {commentExpanded && (
-        <ul className="comment-list">
-          {commentList.map((commentItem) => (
-            <li className="comment-list-item" key={commentItem.commentId}>
-              <span className="comment-item-username">
-                {commentItem.username}
-                {": "}
-              </span>
-              <span className="comment-item-content">
-                {commentItem.content}
-              </span>
-            </li>
-          ))}
-        </ul>
+        <>
+          <ul className="comment-list">
+            {commentList.map((commentItem) => (
+              <li className="comment-list-item" key={commentItem.commentId}>
+                <span className="comment-item-username">
+                  {console.log(commentItem)}
+                  {commentItem.username}
+                  {": "}
+                </span>
+                <span className="comment-item-content">
+                  {commentItem.content}
+                </span>
+              </li>
+            ))}
+          </ul>{" "}
+          <form
+            action=""
+            className="comment-item-input"
+            onSubmit={onSubmitComment}
+          >
+            <label>
+              New comments:
+              <input type="text" name="comment-new" />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        </>
       )}
     </div>
   );
