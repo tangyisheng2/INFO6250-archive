b700733c4d2ca90c7ef622c86c45d5d1dc3cb331
Eason Tang <tangyisheng2@sina.com>
2022-12-11 16:51:19 -0800

    feat: <client> add account-suspended error

--- final/client/src/constants/error-constant.js
+++ final/client/src/constants/error-constant.js
@@ -3,6 +3,7 @@ const ErrorConstant = {
   "permission-denied":
     "You don't have sufficient permission to do this operation",
   "user-exist": "The username is already been used, pick another one",
+  "account-suspended": "Your account is banned.",
 };
 
 export default ErrorConstant;
